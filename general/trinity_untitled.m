99152c1a4a1d04610417ae09492145de9a6ed265
Joachim Vandekerckhove <joachim@uci.edu>
2015-03-07 14:25:48 -0800

    Changed .cmd extension to .script

--- general/trinity_untitled.m
+++ general/trinity_untitled.m
@@ -1,5 +1,6 @@
 function name = trinity_untitled(options, style, idx)
 % TRINITY_UNTITLED  Makes unique if unimaginative names for temporary files
+%   TRINITY_UNTITLED(options, style, idx)
 
 % (c)2013- Joachim Vandekerckhove. See license.txt for licensing information.
 
@@ -10,59 +11,61 @@ initfilename    = options.initfilename;
 scriptfilename  = options.scriptfilename;
 logfilename     = options.logfilename;
 
+ext.model  = engine;
+ext.script = 'script';
+ext.log    = 'log';
+ext.data   = 'data';
+ext.init   = 'init';
+
 switch style
     case 'model'
         if ~isempty(modelfilename)
             name = modelfilename;
         else
             ctr = 0;
-            name = sprintf('untitled.%s', engine);
+            name = sprintf('untitled.%s', ext.model);
             while exist(name, 'file')
                 ctr = ctr + 1;
-                name = sprintf('untitled%i.%s', ctr, engine);
+                name = sprintf('untitled%i.%s', ctr, ext.model);
             end
         end
-        name = force_path_and_extension(name, style, options);
     case 'script'
         if ~isempty(scriptfilename)
             if ischar(initfilename)  % if root given
-                name = sprintf('%s_%i.cmd', scriptfilename, idx);
+                name = sprintf('%s_%i.%s', scriptfilename, idx, ext.script);
             else
                 name = scriptfilename{idx};
             end
         else
             ctr = 0;
-            name = sprintf('untitled_%i.cmd', idx);
+            name = sprintf('untitled_%i.%s', idx, ext.script);
             while exist(name, 'file')
                 ctr = ctr + 1;
-                name = sprintf('untitled%i_%i.cmd', ctr, idx);
+                name = sprintf('untitled%i_%i.%s', ctr, idx, ext.script);
             end
         end
-        name = force_path_and_extension(name, style, options);
     case 'log'
         if ~isempty(logfilename)
             name = logfilename;
         else
             ctr = 0;
-            name = sprintf('untitled_%i.log', idx);
+            name = sprintf('untitled_%i.%s', idx, ext.log);
             while exist(name, 'file')
                 ctr = ctr + 1;
-                name = sprintf('untitled%i_%i.log', ctr, idx);
+                name = sprintf('untitled%i_%i.%s', ctr, idx, ext.log);
             end
         end
-        name = force_path_and_extension(name, style, options);
     case 'data'
         if ~isempty(datafilename)
             name = datafilename;
         else
             ctr = 0;
-            name = 'untitled.data';
+            name = script('untitled.%s', ext.data);
             while exist(name, 'file')
                 ctr = ctr + 1;
-                name = sprintf('untitled%i.data', ctr);
+                name = sprintf('untitled%i.%s', ctr, ext.data);
             end
         end
-        name = force_path_and_extension(name, style, options);
     case 'init'
         if ~isempty(initfilename)
             if ischar(initfilename)  % if root given
@@ -72,41 +75,29 @@ switch style
             end
         else
             ctr = 0;
-            name = sprintf('untitled_%i.init', idx);
+            name = sprintf('untitled_%i.%s', idx, ext.init);
             while exist(name, 'file')
                 ctr = ctr + 1;
-                name = sprintf('untitled%i_%i.init', ctr, idx);
+                name = sprintf('untitled%i_%i.%s', ctr, idx, ext.init);
             end
         end
-        name = force_path_and_extension(name, style, options);
     otherwise
         error_tag('trinity:trinity_untitled:unknownstyle', ...
             'Unknown filename style "%s".', style)
 end
 
+name = force_path_and_extension(name, style, options, ext);
+
 end
 
 
 %% --------------------------------------------------------------------- %%
-function name = force_path_and_extension(name, style, options)
+function name = force_path_and_extension(name, style, options, ext)
 
-engine     = options.engine;
 workingdir = options.workingdir;
 
 [~, file, ~] = fileparts(name);
-switch style
-    case 'model'
-        ext = engine;
-    case 'script'
-        ext = 'cmd';
-    case 'data'
-        ext = 'data';
-    case 'init'
-        ext = 'init';
-    case 'log'
-        ext = 'log';
-end
 
-name = fullfile(workingdir, [file '.' ext]);
+name = fullfile(workingdir, [file '.' ext.(style)]);
 
 end
